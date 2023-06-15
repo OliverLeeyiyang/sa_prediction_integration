ca67b01241d2dd3c5142bbf1bd896e1c7107183f
Yiyang <ge89yes@tum.de>
2023-06-15 22:05:00 +0200

    new file:   cpp_to_python/CMakeLists.txt 	new file:   cpp_to_python/include/cpp_to_python/dealBinMsg.h 	new file:   cpp_to_python/package.xml 	new file:   cpp_to_python/src/dealBinMsg.cpp

---
+++ cpp_to_python/include/cpp_to_python/dealBinMsg.h
@@ -0,0 +1,6 @@
+#include "lanelet2_extension/utility/message_conversion.hpp"
+#include <boost/archive/binary_iarchive.hpp>
+#include <lanelet2_core/primitives/Lanelet.h>
+
+#include <sstream>
+#include <string>
